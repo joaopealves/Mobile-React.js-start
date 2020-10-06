832866c6517fcf810b2d982868c9d47e3ad0b70c
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-06 12:19:55 -0300

    Ending screen

--- Reactjs-mobile/App.js
+++ Reactjs-mobile/App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Linking } from 'react-native';
+import logoIcon from './assets/logo512.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={{
+          height: '40vmin',
+          width: '40vmin',
+          pointerEvents: 'none',
+        }}
+        source={logoIcon}
+      />
+      <Text
+        style={{
+          color: '#fff',
+          fontSize: 'calc(10px + 2vmin)',
+        }}
+      >
+        Edit src/App.js and save to reload.
+      </Text>
+      <Text
+        style={{
+          color: 'rgb(97, 218, 251)',
+          fontSize: 'calc(10px + 2vmin)',
+          textDecorationColor: 'rgb(97, 218, 251)',
+          textDecorationLine: 'underline',
+        }}
+        onPress={() => Linking.openURL('https://reactjs.org')}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +43,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(40, 44, 52)',
     alignItems: 'center',
     justifyContent: 'center',
   },
