745ab6bcb04e84c3f6a04b76e55c73ca1c2ac6c1
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-06 13:44:11 -0300

    Issue on Class

--- Reactjs-mobile/App.js
+++ Reactjs-mobile/App.js
@@ -1,16 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image, Linking } from 'react-native';
+import React, {Component} from 'react';
+import { AppRegistry, StyleSheet, Text, View, Image, Linking, Animated } from 'react-native';
 import logoIcon from './assets/logo512.png';
 
 export default function App() {
+
+  componentWillMount() {
+    this.animatedValue = new Animated.Value(0);
+  }
+  render();
+  const interpolateRotation = this.animatedValue.interpolate({
+    inputRange: [0,1],
+    outputRange: ['0deg', '360deg'],
+  })
+  const animatedStyle = {
+    transform: [
+      {rotate: interpolateRotation}
+    ]
+  }
   return (
     <View style={styles.container}>
       <Image
         style={{
+          transform: [{ rotate: spin }],
           height: '40vmin',
           width: '40vmin',
           pointerEvents: 'none',
+          animation: 'App-logo-spin infinite 20s linear',
         }}
         source={logoIcon}
       />
@@ -22,19 +38,23 @@ export default function App() {
       >
         Edit src/App.js and save to reload.
       </Text>
+      <Animated.View>
+
       <Text
+      style={[animatedStyle]}
         style={{
           color: 'rgb(97, 218, 251)',
           fontSize: 'calc(10px + 2vmin)',
           textDecorationColor: 'rgb(97, 218, 251)',
           textDecorationLine: 'underline',
         }}
-        onPress={() => Linking.openURL('https://reactjs.org')}
+        onPress={() => Linking.openURL('https://reactnative.dev')}
         target="_blank"
         rel="noopener noreferrer"
-      >
+        >
         Learn React
       </Text>
+        </Animated.View>
       <StatusBar style="auto" />
     </View>
   );
